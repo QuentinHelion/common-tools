a992e94c7801937570d1dce2b820e981c252ae95
QuentinHelion <helion.quentin@gmail.com>
2023-08-09 15:31:53 +0200

    functionnal arch

---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+
+const Header = () => {
+    return (
+        <header>
+            <Navigation/>
+            <h1>Welcome to My App</h1>
+        </header>
+    );
+};
+
+export default Header;
