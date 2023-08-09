11f3460dee638b87d33914bc573843248fcdb903
QuentinHelion <helion.quentin@gmail.com>
2023-08-10 01:42:10 +0200

    add navigation styles

--- src/components/layout/Navigation/Navigation.js
+++ src/components/layout/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Navigation.css';
 import NavLink from '../../../cummon/NavLink/NavLink';
 function Navigation() {
     return (
